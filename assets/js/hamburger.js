3f8e41b0d473fa9e6f0c2337b6b4f237488198f2
Parth Krishna <thenerdsuperuser@gmail.com>
2020-05-12 05:14:19 +0530

    Added new media query parameters

---
+++ assets/js/hamburger.js
@@ -0,0 +1,11 @@
+$('#ChangeToggle').click(function () {
+	if ($('#ChangeToggle span').hasClass('ToggleButton')) {
+		$('#ChangeToggle').html('<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
+
+	}
+	else {
+		$('#ChangeToggle').html('<span class="ToggleButton glyphicon glyphicon-remove"></span>');
+
+	}
+
+});
